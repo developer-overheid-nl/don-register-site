ac9614eb67d0f7a4c93c21a63eaeccdbd6bc6acf
Jaap-Hein Wester <j.h.wester@setonix.nl>
2025-06-12 19:29:39 +0200

    WIP: added Rijkshuisstijl community packages and fonts.

---
+++ packages/components/src/Test.tsx
@@ -0,0 +1,8 @@
+// import React from 'react';
+import { Link } from '@rijkshuisstijl-community/components-react';
+import '@rijkshuisstijl-community/components-css/dist/index.css';
+import '@rijkshuisstijl-community/design-tokens/dist/root.css';
+
+export default function Test() {
+  return <div>Test <Link href="#">Link</Link></div>;
+}
