7cb185be19788206170799c47e73456df2839184
Jaap-Hein Wester <j.h.wester@setonix.nl>
2025-09-23 11:34:46 +0200

    WIP: add openapi-fetch and schema types, api openapi.son is still 404

---
+++ apps/api-register/src/types/unknown-schema.ts
@@ -0,0 +1,17 @@
+/**
+ * This file is a fallback.
+ * Put the oas url of the api in `[root]/redocly.yaml` and run `ts:oas`.
+ */
+
+export type paths = Record<string, unknown>;
+export type webhooks = Record<string, unknown>;
+export interface components {
+    schemas: unknown;
+    responses: unknown;
+    parameters: unknown;
+    requestBodies: unknown;
+    headers: unknown;
+    pathItems: unknown;
+}
+export type $defs = Record<string, unknown>;
+export type operations = Record<string, unknown>;
