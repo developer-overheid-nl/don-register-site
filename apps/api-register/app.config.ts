ee810294f707c4b2ab45629d590d923ddbd241a3
Jaap-Hein Wester <j.h.wester@setonix.nl>
2025-09-30 20:56:29 +0200

    WIP: Footer links added

--- apps/api-register/app.config.ts
+++ apps/api-register/app.config.ts
@@ -31,4 +31,38 @@ export default {
       { id: 'oss', label: "Open Source", href: "https://oss.developer.overheid.nl", icon: "_external", target: "_blank" },
     ],
   },
+  footer: {
+    columns: [
+      {
+        title: "Medemogelijk gemaakt door",
+        text: 
+`
+![BZK](/sponsors/bzk.svg)  
+![VNG](/sponsors/vng.svg)  
+![FS](/sponsors/forumstandaardisatie.svg)  
+`,
+      },
+      {
+        title: "Community",
+        items: [
+          { id: 'slack', label: "Slack", href: "https://codefornl.slack.com/archives/CFV4B3XE2", icon: "_external", target: "_blank" },
+          { id: 'github', label: "Github", href: "https://github.com/developer-overheid-nl", icon: "_external", target: "_blank" },
+          { id: 'mastodon', label: "Mastodon", href: "https://social.overheid.nl/@developer", icon: "_external", target: "_blank" },
+          { id: 'linkedin', label: "LinkedIn", href: "https://nl.linkedin.com/company/developer-overheid-nl", icon: "_external", target: "_blank" },
+        ]
+      },
+      {
+        title: "Overig",
+        items: [
+          { id: 'bijdragen', label: "Bijdragen", href: "https://developer.overheid.nl/contributing" },
+          { id: 'implos', label: "Implementatie ondersteuning", href: "https://developer.overheid.nl/implementatie-ondersteuning" },
+          { id: 'contact', label: "Contact", href: "https://developer.overheid.nl/contact" },
+          { id: 'team', label: "Het team", href: "https://developer.overheid.nl/team" },
+          { id: 'privacy', label: "Privacyverklaring", href: "https://developer.overheid.nl/privacy" },
+          { id: 'toegankelijkheid', label: "Toegankelijkheid", href: "https://developer.overheid.nl/toegankelijkheid" },
+          { id: 'archive', label: "Sitearchief", href: "https://minbzk.sitearchief.nl/?subsite=devoverheidapis#archive", icon: "_external", target: "_blank" },
+        ]
+      }
+    ]
+  },
 };
