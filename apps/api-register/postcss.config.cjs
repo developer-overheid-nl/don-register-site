1f8fc02c9ca5d7582ac7bb385fe13480a69a2bed
Jaap-Hein Wester <j.h.wester@setonix.nl>
2025-10-16 14:47:29 +0200

    Responsive layouts

---
+++ apps/api-register/postcss.config.cjs
@@ -0,0 +1,10 @@
+const postcssGlobalData = require('@csstools/postcss-global-data');
+
+module.exports = {
+  plugins: [
+    postcssGlobalData({
+      files: ['../../packages/layouts/src/styles/breakpoints.css']
+    }),
+    require('postcss-custom-media'),
+  ],
+}
