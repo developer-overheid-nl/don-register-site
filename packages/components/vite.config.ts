917771d42e8d29a8e4e2eee48e2b75d1683d3324
Jaap-Hein Wester <j.h.wester@setonix.nl>
2026-02-10 15:56:34 +0100

    Cleanup

--- packages/components/vite.config.ts
+++ packages/components/vite.config.ts
@@ -1,3 +1,4 @@
+// OBSOLETE or do we want to publish a bundled version? 
 import { dirname, resolve } from "node:path";
 import { fileURLToPath } from "node:url";
 import react from "@vitejs/plugin-react";
@@ -16,17 +17,11 @@ export default defineConfig({
         i18n: resolve(__dirname, "src/i18n.ts"),
       },
       name: "DON Components",
-      // fileName: "components",
       cssFileName: "styles",
     },
     ssr: "src/index.ts",
     ssrEmitAssets: true,
     rollupOptions: {
-      // input: {
-      //   index: resolve(__dirname, "src/index.ts"),
-      //   client: resolve(__dirname, "src/client.ts"),
-      //   i18n: resolve(__dirname, "src/i18n.ts"),
-      // },
       external: [
         "react",
       ],
