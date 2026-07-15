691daf3679aa1a9aa25e8d6d7a3d611311f52b50
Jaap-Hein Wester <j.h.wester@setonix.nl>
2026-07-15 13:41:34 +0200

    More storybook and chromatic setup; extra stories

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -5,26 +5,33 @@ import { mergeConfig } from "vite";
 
 const config: StorybookConfig = {
   stories: ["../packages/components/src/**/*.stories.@(ts|tsx)"],
-  addons: ["@storybook/addon-docs"],
+  addons: [
+    "@storybook/addon-docs",
+    "@storybook/addon-vitest",
+    "@storybook/addon-a11y",
+    "@chromatic-com/storybook",
+  ],
   framework: {
     name: "@storybook/react-vite",
     options: {},
   },
-  docs: {
-    autodocs: "tag",
+  typescript: {
+    reactDocgen: "react-docgen-typescript",
+    reactDocgenTypescriptOptions: {
+      shouldExtractLiteralValuesFromEnum: true,
+      shouldRemoveUndefinedFromOptional: true,
+    },
   },
+  // docs: {
+  //   autodocs: "tag",
+  // },
   async viteFinal(config) {
     return mergeConfig(config, {
       css: {
         postcss: {
           plugins: [
             postcssGlobalData({
-              files: [
-                new URL(
-                  "../packages/layouts/src/styles/breakpoints.css",
-                  import.meta.url,
-                ).pathname,
-              ],
+              files: ["./packages/layouts/src/styles/breakpoints.css"],
             }),
             postcssCustomMedia(),
           ],
---
+++ packages/components/src/copyButton/CopyButton.stories.tsx
@@ -0,0 +1,36 @@
+import type { Meta, StoryObj } from "@storybook/react-vite";
+import { expect, fn } from "storybook/test";
+
+import CopyButton from "./CopyButton";
+
+const meta: Meta<typeof CopyButton> = {
+  title: "Components/CopyButton",
+  component: CopyButton,
+  tags: ["autodocs"],
+};
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    text: "This text will be copied!",
+  },
+  play: async ({ canvas, userEvent, args }) => {
+    const cpButton = canvas.getByTitle("Kopieer naar klembord");
+
+    const writeTextMock = fn();
+    Object.defineProperty(navigator, "clipboard", {
+      value: { writeText: writeTextMock },
+      writable: true,
+      configurable: true,
+    });
+
+    await userEvent.click(cpButton);
+
+    await expect(writeTextMock).toHaveBeenCalledWith(args.text);
+
+    writeTextMock.mockRestore();
+  },
+};
