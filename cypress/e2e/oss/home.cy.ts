84c3789752c65358e544aae74a5503c0ee1f18c8
Jaap-Hein Wester <j.h.wester@setonix.nl>
2026-07-22 15:25:00 +0200

    Refactor Cypress tests for API-register and Open Source Register; add keyRequest fixture

--- cypress/e2e/oss/home.cy.ts
+++ cypress/e2e/oss/home.cy.ts
@@ -5,6 +5,59 @@ describe("oss register", () => {
     cy.visit("/");
 
     cy.get("main").should("be.visible");
-    cy.title().should("not.be.empty");
+    cy.title().should("eq", "Overzicht | Open Source Register");
+
+    cy.get(".results ol li").should("have.length", 20);
+    cy.get(".filters #facetfilters input").should("have.length.above", 1);
+  });
+
+  it("can navigate to the next page", () => {
+    cy.visit("/repositories/pagina/1");
+
+    cy.get('[aria-label="Paginering"] a').should("have.length.above", 3);
+    cy.get('[rel="next"]').click();
+
+    cy.location("pathname").should("match", /\/pagina\/2/);
+    cy.get('[aria-current="true"]')
+      .should("have.text", "2")
+      .and("have.attr", "aria-label", "Pagina 2: Resultaten 21 tot en met 40");
+  });
+
+  it.only("can filter items", () => {
+    cy.visit("/");
+
+    cy.get("#facetfilters input").first().as("firstFilter");
+    cy.get("#facetfilters input").last().as("lastFilter");
+    cy.get("#get-filters").as("filtersForm");
+    cy.get('[aria-label="Huidige filters"] a').should("have.length", 1);
+
+    cy.get("@firstFilter").scrollIntoView().focus().press("ArrowRight");
+    cy.get("@filtersForm").submit();
+
+    cy.get("@firstFilter").then((filter) => {
+      const name = filter.attr("name");
+      cy.get(`input[name="${name}"]:checked`).then((inputChecked) => {
+        const value = inputChecked.val();
+
+        const re = new RegExp(`${name}=${value}`, "g");
+        cy.location("search").should("match", re);
+      });
+    });
+
+    cy.get('[aria-label="Huidige filters"] a').should("have.length", 1);
+
+    cy.get("@lastFilter").scrollIntoView().check();
+    cy.get("@filtersForm").submit();
+
+    cy.get('[aria-label="Huidige filters"] a').should("have.length", 3);
+  });
+
+  it("loads the toevoegen page", () => {
+    cy.visit("/repositories/toevoegen");
+
+    cy.get(".basic-content").should("be.visible");
+    cy.get(
+      'a[href="https://apis.developer.overheid.nl/apis/key-aanvragen"]',
+    ).should("have.length.at.least", 1);
   });
 });
--- cypress/support/e2e.ts
+++ cypress/support/e2e.ts
@@ -1,1 +1,7 @@
 // Shared Cypress e2e support can be registered here.
+
+beforeEach(() => {
+  cy.intercept("https://statistiek.rijksoverheid.nl/ppms.*", { log: false }).as(
+    "piwikPing",
+  );
+});
